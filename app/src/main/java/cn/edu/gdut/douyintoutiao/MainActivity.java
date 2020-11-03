6f87192ac0c8ef7eb0f1bf9a6d4fa05e7b020f12
Hayring <KID141220@gmail.com>
2020-11-03 23:59:59 +0800

    user main activity demo

--- app/src/main/java/cn/edu/gdut/douyintoutiao/MainActivity.java
+++ app/src/main/java/cn/edu/gdut/douyintoutiao/MainActivity.java
@@ -2,13 +2,25 @@ package cn.edu.gdut.douyintoutiao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button userMainPageEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userMainPageEnter = findViewById(R.id.user_main_page_enter);
+        userMainPageEnter.setOnClickListener((view -> {
+            Intent intent = new Intent();
+            intent.setClass(this,UserMainActivity.class);
+            MainActivity.this.startActivity(intent);
+        }));
     }
 }
