9c2fb73a12b819fbb4c49b3c060b4d42042b38e6
Hayring <KID141220@gmail.com>
2020-11-11 13:02:09 +0800

    fix merge bugs

--- app/src/main/java/cn/edu/gdut/douyintoutiao/view/ResignFragment.java
+++ app/src/main/java/cn/edu/gdut/douyintoutiao/view/ResignFragment.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+
 import cn.edu.gdut.douyintoutiao.databinding.FragmentResignBinding;
 import es.dmoral.toasty.Toasty;
 
