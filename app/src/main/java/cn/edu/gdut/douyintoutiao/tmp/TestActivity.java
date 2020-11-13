73e4267bdf4b02e902258cd8cba811522730e77d
ZJPang <516585610@qq.com>
2020-11-13 17:13:43 +0800

    fixed 编译失败问题

--- app/src/main/java/cn/edu/gdut/douyintoutiao/tmp/TestActivity.java
+++ app/src/main/java/cn/edu/gdut/douyintoutiao/tmp/TestActivity.java
@@ -7,7 +7,6 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import cn.edu.gdut.douyintoutiao.R;
-import cn.edu.gdut.douyintoutiao.view.show.text.NewsActivity;
 import cn.edu.gdut.douyintoutiao.view.show.video.FullscreenActivity;
 
 public class TestActivity extends AppCompatActivity {
@@ -15,7 +14,6 @@ public class TestActivity extends AppCompatActivity {
 
     Button playVideoButton;
 
-    Button buttonToNews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +23,6 @@ public class TestActivity extends AppCompatActivity {
 
         playVideoButton = findViewById(R.id.play_video_button);
 
-
-        buttonToNews.setOnClickListener(view -> {
-            Intent intent = new Intent(this, NewsActivity.class);
-            startActivity(intent);
-        });
 
         playVideoButton.setOnClickListener(view -> {
             Intent intent = new Intent(this, FullscreenActivity.class);
