ec20d538606f081a262bc163d7bd0087df1a4d10
ZJPang <516585610@qq.com>
2020-11-16 22:43:34 +0800

    save commit

--- app/src/main/java/cn/edu/gdut/douyintoutiao/view/show/search/view/SearchFragment.java
+++ app/src/main/java/cn/edu/gdut/douyintoutiao/view/show/search/view/SearchFragment.java
@@ -1,21 +1,24 @@
 package cn.edu.gdut.douyintoutiao.view.show.search.view;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import cn.edu.gdut.douyintoutiao.databinding.SearchFragmentBinding;
-import cn.edu.gdut.douyintoutiao.view.show.search.viewmodel.SearchViewModel;
+import es.dmoral.toasty.Toasty;
 
+/**
+ * @author cypang
+ */
 public class SearchFragment extends Fragment {
 
-    private SearchViewModel mViewModel;
 
     private SearchFragmentBinding binding;
 
@@ -33,9 +36,28 @@ public class SearchFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
-        // TODO: Use the ViewModel
+        //默认展开搜索框并且打开输入键盘
+        binding.searchView.onActionViewExpanded();
+        //显示搜索提示
+        binding.searchView.setQueryHint("请输入你要搜索的信息");
+        //显示搜索按键
+        binding.searchView.setSubmitButtonEnabled(true);
+        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (TextUtils.isEmpty(query)) {
+                    Toasty.warning(requireContext(), "请输入搜索内容").show();
+                    return false;
+                }
+                Toasty.normal(requireContext(), query, Toasty.LENGTH_SHORT).show();
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
 }
--- app/src/main/java/cn/edu/gdut/douyintoutiao/view/show/search/viewmodel/SearchViewModel.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.gdut.douyintoutiao.view.show.search.viewmodel;
-
-import androidx.lifecycle.ViewModel;
-
-public class SearchViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
