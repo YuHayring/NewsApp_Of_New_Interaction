7aa557a563443fde8022f0f5a374e33f8adb02f0
cypang <516585610@qq.com>
2020-12-04 00:11:31 +0800

    修复评论后不及时刷新页面问题

--- app/src/main/java/cn/edu/gdut/douyintoutiao/view/show/comment/model/CommentRepository.java
+++ app/src/main/java/cn/edu/gdut/douyintoutiao/view/show/comment/model/CommentRepository.java
@@ -5,13 +5,21 @@ import android.util.Log;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import cn.edu.gdut.douyintoutiao.base.ObserverManager;
 import cn.edu.gdut.douyintoutiao.entity.Discuss;
 import cn.edu.gdut.douyintoutiao.entity.Result;
+import cn.edu.gdut.douyintoutiao.entity.User;
 import cn.edu.gdut.douyintoutiao.net.CommentApi;
+import cn.edu.gdut.douyintoutiao.view.show.comment.CommentCallBack;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -26,6 +34,12 @@ public class CommentRepository {
     private final MutableLiveData<List<Discuss>> allDiscussData;
     private static final String TAG = "comment";
     private final CommentApi api;
+
+    private CommentCallBack callBack;
+
+    public void setCallBack(CommentCallBack callBack) {
+        this.callBack = callBack;
+    }
 
     public CommentRepository(CommentApi api) {
         this.allDiscussData = new MutableLiveData<>();
@@ -55,18 +69,30 @@ public class CommentRepository {
         map.put("newsId", newsID);
         map.put("userId", userID);
         map.put("content", content);
-        Call<Result<Void>> resultCall = api.postComment(map);
-        resultCall.enqueue(new Callback<Result<Void>>() {
-            @Override
-            public void onResponse(Call<Result<Void>> call, Response<Result<Void>> response) {
-                Log.d(TAG, "onResponse: 请求成功" + response.body().toString());
-            }
+        Observable<Result<Void>> resultCall = api.postComment(map);
+        resultCall.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new ObserverManager<Result<Void>>() {
+                    @Override
+                    public void onSuccess(Result<Void> userResult) {
+                        Log.d(TAG, "onSuccess: " + userResult.toString());
+                    }
 
-            @Override
-            public void onFailure(Call<Result<Void>> call, Throwable t) {
-                Log.d(TAG, "onFailure: 请求失败");
-            }
-        });
+                    @Override
+                    public void onFail(Throwable throwable) {
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        Log.d(TAG, "onFinish: 请求完成");
+                        callBack.updateComment();
+                    }
+
+                    @Override
+                    public void onDisposable(Disposable disposable) {
+
+                    }
+                });
     }
 
 
