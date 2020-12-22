cf2b68715f9ae1109f824482790cfe0c4c1708b9
Hayring <KID141220@gmail.com>
2020-12-22 20:48:10 +0800

    滑动灵敏度终极版

--- app/src/main/java/cn/edu/gdut/douyintoutiao/view/utilview/ScrollHelperFrameLayout.java
+++ app/src/main/java/cn/edu/gdut/douyintoutiao/view/utilview/ScrollHelperFrameLayout.java
@@ -2,6 +2,7 @@ package cn.edu.gdut.douyintoutiao.view.utilview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.FrameLayout;
 
@@ -32,44 +33,87 @@ public class ScrollHelperFrameLayout extends FrameLayout {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
+    /**
+     * 滑动方向找到之前的次数
+     */
+    int findTimes;
 
+    /**
+     * 多少次决定滑动方向
+     */
+    private static final int FIND_TIMES = 3;
 
-    private int startX, startY;
+    /**
+     * 触发坐标 x
+     */
+    int startX;
 
-
+    /**
+     * 触发坐标 y
+     */
+    int startY;
     /**
      * 是否已找到当前滑动状态
      */
     private boolean findCurrent = false;
 
+    boolean vertical;
+
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
+//        Log.d("OnTouch","run");
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                findCurrent = false;
+                //重置寻找次数
+                findTimes = 0;
+                //重置
                 startX = (int) ev.getX();
                 startY = (int) ev.getY();
                 getParent().requestDisallowInterceptTouchEvent(true);
+                findTimes++;
                 break;
             case MotionEvent.ACTION_MOVE:
-                if (!findCurrent) {
+                if (findTimes > FIND_TIMES) {
+                    break;
+                }
+                if (findTimes < FIND_TIMES) {
+                    findTimes++;
+                } else {
                     int endX = (int) ev.getX();
                     int endY = (int) ev.getY();
                     int disX = Math.abs(endX - startX);
                     int disY = Math.abs(endY - startY);
-                    if(disX >  DouYinTouTiaoApplication.SCROLL_TAN * disY){
+                    if (disX > DouYinTouTiaoApplication.SCROLL_TAN * disY) {
+                        //横向滑动
+                        vertical = false;
                         getParent().requestDisallowInterceptTouchEvent(false);
-                    }else {
+                        Log.i("滑动方向","横向");
+                        return false;
+                    } else {
+                        //竖向滑动
+                        vertical = true;
                         getParent().requestDisallowInterceptTouchEvent(true);
+                        Log.i("滑动方向","竖向");
                     }
-                    findCurrent = true;
+                    findTimes++;
                 }
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                getParent().requestDisallowInterceptTouchEvent(false);
-                break;
+                if (vertical) {
+                    getParent().requestDisallowInterceptTouchEvent(false);
+                } else {
+                    vertical = true;
+                }
+
         }
         return super.dispatchTouchEvent(ev);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        if (!vertical) return false;
+        return super.onTouchEvent(e);
     }
 }
